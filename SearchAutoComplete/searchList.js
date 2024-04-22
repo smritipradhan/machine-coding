7a25fa221d01237b6aa14b35137386bbe2894e6d
smritipradhan <smritipradhan545@gmail.com>
2024-04-23 00:08:05 +0530

    search autocomplete

--- SearchAutoComplete/searchList.js
+++ SearchAutoComplete/searchList.js
@@ -1,4 +1,4 @@
-const searchList = [
+export const searchList = [
   { title: "The quick brown fox jumps over the lazy dog." },
   { title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
   { title: "A stitch in time saves nine." },
@@ -28,31 +28,3 @@ const searchList = [
   { title: "When in Rome, do as the Romans do." },
   { title: "You reap what you sow." },
 ];
-
-const inputField = document.getElementById("inputField");
-const resultField = document.getElementById("result");
-let inputFieldValue = "";
-let finalList = [];
-
-inputField.addEventListener("input", () => {
-  inputFieldValue = inputField.value;
-
-  while (resultField.firstChild) {
-    resultField.removeChild(resultField.firstChild);
-  }
-
-  if (inputFieldValue) {
-    finalList = searchList.filter((searchItem) =>
-      searchItem.title.toLowerCase().includes(inputFieldValue.toLowerCase())
-    );
-
-    finalList.forEach((item) => {
-      const div = document.createElement("div");
-      div.textContent = item.title;
-      resultField.appendChild(div);
-    });
-  } else {
-    const div = document.createElement("div");
-    resultField.appendChild(div);
-  }
-});
